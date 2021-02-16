0953a818900530bd832b586933922beb091d768d
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2021-02-16 19:03:51 +0100

    Change buttons in projects's cards (fix #45)

--- src/js/components/projects.js
+++ src/js/components/projects.js
@@ -13,8 +13,8 @@ const projects = {
                         <h4 class="mb-0">{{work.title}}</h4>
                         <span class="card-subtitle text-muted">{{work.stack}}</span>
 						<p class="mt-3">{{work.moreInfos}}</p>
-						<a :href="work.url" target="_blank" class="btn btn-lg btn-main-light mr-3">Voir le projet</a>
-						<a :href="work.github" target="_blank" class="btn btn-lg btn-main-light"><i class="lab la-github"></i></a>
+            <a :href="work.url" target="_blank" class="btn btn-lg btn-main font-main mr-3">Voir le projet<i class="las la-eye ml-2"></i></a>
+						<a :href="work.github" target="_blank" class="btn btn-lg btn-main-light">Voir le code<i class="lab la-github ml-2"></i></a>
 					</div>
 				</div>
 				<div class="col-12 col-md-6 card border-0 bg-transparent animated-fadeInLeft">
