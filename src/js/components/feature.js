2f23e379b26e3d6097ddd6024ff7c2ce2d1068ca
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2021-02-27 22:31:01 +0100

    Update feature text

--- src/js/components/feature.js
+++ src/js/components/feature.js
@@ -11,7 +11,11 @@ const feature = {
 				<div class="col-12 col-md-8 card border-0 bg-transparent animated-fadeInRight">
 					<div class="py-5">
 						<span class="d-block mb-4 font-main font-15">Salut, je suis Lionel Ensfelder</span>
-						<h1 class="">Ancien technicien informatique de la Marine Nationale, je suis maintenant développeur frontend Javascript.</h1>
+            <h1>Je préfère la qualité à la quantité...<br />Je prends le temps qu'il faut pour faire de l'exercice. Je bichonne mon sommeil.</h1>
+            <blockquote class="blockquote">
+              <p class="mb-0">La simplicité est la sophistication suprême.</p>
+              <footer class="blockquote-footer bg-transparent">Léonard de Vinci</footer>
+            </blockquote> 
 						<a href="assets/documents/CV-LIONEL-ENSFELDER.pdf" target="_blank" class="btn btn-lg btn-main font-main mr-3">CV<i class="las la-file-pdf ml-2"></i></a>
 						<a href="https://github.com/LionelENSFELDER?tab=repositories" target="_blank" class="btn btn-lg btn-main-light my-4 mr-3">Github<i class="lab la-github ml-2"></i></a>
             <a href="https://www.linkedin.com/in/lionel-ensfelder/" target="_blank" class="btn btn-lg btn-main-light">Linkedin<i class="lab la-linkedin ml-2"></i></a>
